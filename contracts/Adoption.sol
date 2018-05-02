4055e34f6811dcec1204fa1842ae1a11b7cb5c8a
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2018-05-02 16:15:10 +0900

    Added function to return entire array.

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -10,4 +10,9 @@ contract Adoption {
 
         return petId;
     }
+
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16]) {
+        return adopters;
+    }
 }
